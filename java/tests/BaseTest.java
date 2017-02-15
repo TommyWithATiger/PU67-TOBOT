8b7e497d5f7c2ea1b94c7c5fcd10deec9dd63391
Espen Haugsdal <espen.haugsdal@gmail.com>
2017-02-15 18:59:33 +0100

    remove TestCase inheritance.
    TestCase inheritance forces old jUnit version that does not respect
    @before and @ after annotations.

--- java/tests/BaseTest.java
+++ java/tests/BaseTest.java
@@ -1,9 +1,11 @@
 import junit.framework.TestCase;
+import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Ignore;
 
 @Ignore
-public class BaseTest extends TestCase {
+public class BaseTest{
+
     @BeforeClass
     public static void setUpBaseClass(){
         System.setProperty("javax.xml.accessExternalDTD", "all");
