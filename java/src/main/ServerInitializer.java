69ee4736565229286b5cfb4e04054ba36f6f97eb
Espen Haugsdal <espen.haugsdal@gmail.com>
2017-02-15 13:51:53 +0100

    Added ServerInitializer
    This will start the server.

---
+++ java/src/main/ServerInitializer.java
@@ -0,0 +1,9 @@
+package main;
+
+public class ServerInitializer {
+    public static void main(String[] args) {
+        System.setProperty("javax.xml.accessExternalDTD", "all");
+
+        // TODO: do actual stuff here
+    }
+}
