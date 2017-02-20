fdb3b5fbb2cf4787f8673baf075b86fdfe57b64c
TommyWithATiger <halvard.hummel@hotmail.com>
2017-02-20 11:23:28 +0100

    Updated ServerThread to use the HttpRequestParser

--- java/src/api/connection/ServerThread.java
+++ java/src/api/connection/ServerThread.java
@@ -1,27 +1,37 @@
 package api.connection;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
+import org.apache.http.HttpException;
+import org.apache.http.HttpRequest;
+import org.apache.http.impl.io.DefaultHttpRequestParser;
+import org.apache.http.impl.io.HttpTransportMetricsImpl;
+import org.apache.http.impl.io.SessionInputBufferImpl;
 
 public class ServerThread extends Thread {
 
   private Socket clientSocket;
-  private BufferedReader inputReader;
   private PrintWriter printWriter;
+  private DefaultHttpRequestParser requestParser;
 
   public ServerThread(Socket clientSocket) throws IOException {
     this.clientSocket = clientSocket;
-    inputReader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+    SessionInputBufferImpl sessionInputBuffer = new SessionInputBufferImpl(
+        new HttpTransportMetricsImpl(), 8192);
+    sessionInputBuffer.bind(clientSocket.getInputStream());
+    requestParser = new DefaultHttpRequestParser(sessionInputBuffer);
     printWriter = new PrintWriter(clientSocket.getOutputStream());
   }
 
   @Override
   public void run() {
-    while(!clientSocket.isClosed()){
-      
+    while (!clientSocket.isClosed()) {
+      try {
+        HttpRequest request = requestParser.parse();
+      } catch (IOException | HttpException e) {
+        e.printStackTrace();
+      }
     }
   }
 
