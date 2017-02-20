e233fbd23ce36e112340f1ccf8bb27cdc60d3350
Niklas Molnes Hole <nikasmh@hotmail.com>
2017-02-20 10:04:36 +0100

    Added rxjs to frontend

--- web/src/main.js
+++ web/src/main.js
@@ -1,6 +1,11 @@
 import Vue from 'vue'
-import App from './App'
+import Rx from 'rxjs/Rx'
+import VueRx from 'vue-rx'
 import router from './router'
+
+import App from './App'
+
+Vue.use(VueRx, Rx)
 
 /* eslint-disable no-new */
 new Vue({
