4f1675832e0e9475aa8e7484156d954b0aafde32
philip <philippuente@hotmail.com>
2017-02-21 12:14:38 +0100

    Added abstract DAO base class

---
+++ java/src/data/DataAccessObjects/DAOBase.java
@@ -0,0 +1,26 @@
+package data.DataAccessObjects;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.PersistenceContext;
+
+public abstract class DAOBase<E, K> {
+
+  protected Class<E> entityClass;
+
+  protected EntityManagerFactory emFactory;
+  @PersistenceContext
+  protected EntityManager entityManager;
+
+  public DAOBase(Class<E> entityClass, EntityManagerFactory emFactory) {
+    this.emFactory = emFactory;
+    this.entityClass = entityClass;
+  }
+
+  public void persist(E entity) { entityManager.persist(entity); }
+
+  public void remove(E entity) { entityManager.remove(entity); }
+
+  public E findById(K id) { return entityManager.find(entityClass, id); }
+
+}
