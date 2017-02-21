2ea69a3a0a1c80173ad645fdff2c0f43477aa9d9
Niklas Molnes Hole <nikasmh@hotmail.com>
2017-02-21 18:59:38 +0100

    Added a logout button component

---
+++ web/src/components/auth/LogoutBtn.vue
@@ -0,0 +1,22 @@
+<template>
+  <button @click="logout">Logg ut</button>
+</template>
+
+<script>
+import { auth } from 'auth'
+
+export default {
+  name: 'logoutbutton',
+  methods: {
+    /**
+     * To log out, we just need to remove the token.
+     */
+    logout () {
+      auth.logout(this)
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
