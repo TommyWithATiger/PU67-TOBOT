a32aaefd1e120a8f4a7406d171b22328639acc92
philip <philippuente@hotmail.com>
2017-02-21 16:41:08 +0100

    Structural changes to DaoBase and TopicDao, Daotests does still not work. Will have to look into it

--- java/src/data/DataAccessObjects/DAOBase.java
+++ java/src/data/DataAccessObjects/DAOBase.java
@@ -2,32 +2,57 @@ package data.DataAccessObjects;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
 import javax.persistence.PersistenceContext;
 
 public abstract class DAOBase<E, K> {
 
   protected Class<E> entityClass;
 
-  protected EntityManagerFactory emFactory;
   @PersistenceContext
-  protected EntityManager entityManager;
+  protected static EntityManagerFactory emFactory;
 
-  public DAOBase(Class<E> entityClass, EntityManagerFactory emFactory) {
-    this.emFactory = emFactory;
-    this.entityManager = emFactory.createEntityManager();
+  public DAOBase(Class<E> entityClass, EntityManagerFactory entityManagerFactory) {
+    this.emFactory = entityManagerFactory;
     this.entityClass = entityClass;
   }
 
   public void persist(E entity) {
+    EntityManager entityManager = emFactory.createEntityManager();
+    EntityTransaction entityTransaction = entityManager.getTransaction();
+    entityTransaction.begin();
     entityManager.persist(entity);
+    entityTransaction.commit();
+    entityManager.close();
+  }
+
+  public E merge(E entity) {
+    EntityManager entityManager = emFactory.createEntityManager();
+    EntityTransaction entityTransaction = entityManager.getTransaction();
+    entityTransaction.begin();
+    entityManager.merge(entity);
+    entityTransaction.commit();
+    entityManager.close();
+    return entity;
   }
 
   public void remove(E entity) {
+    EntityManager entityManager = emFactory.createEntityManager();
+    EntityTransaction entityTransaction = entityManager.getTransaction();
+    entityTransaction.begin();
     entityManager.remove(entity);
+    entityTransaction.commit();
+    entityManager.close();
   }
 
   public E findById(K id) {
-    return entityManager.find(entityClass, id);
+    EntityManager entityManager = emFactory.createEntityManager();
+    EntityTransaction entityTransaction = entityManager.getTransaction();
+    entityTransaction.begin();
+    E entity = entityManager.find(entityClass, id);
+    entityTransaction.commit();
+    entityManager.close();
+    return entity;
   }
 
 }
--- java/tests/data/DataAccessObjects/TopicDAOTest.java
+++ java/tests/data/DataAccessObjects/TopicDAOTest.java
@@ -3,26 +3,38 @@ package data.DataAccessObjects;
 import static junit.framework.TestCase.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.when;
 
 import base.BaseTest;
 import data.Topic;
+import java.util.List;
+import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
-import javax.persistence.Persistence;
+import javax.persistence.EntityTransaction;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
 
+@RunWith(MockitoJUnitRunner.class)
 public class TopicDAOTest extends BaseTest {
 
   @Mock
   private TopicDAO topicDAO;
   @Mock
   private EntityManagerFactory entityManagerFactory;
+  @Mock
+  private EntityManager entityManager;
+  @Mock
+  private EntityTransaction entityTransaction;
 
   @Before
   public void init() {
-    this.entityManagerFactory = Persistence.createEntityManagerFactory("Eclipselink_JPA");
-    this.topicDAO = new TopicDAO(entityManagerFactory);
+    when(entityManagerFactory.createEntityManager()).thenReturn(entityManager);
+    when(entityManager.getTransaction()).thenReturn(entityTransaction);
+    //this.topicDAO = new TopicDAO(entityManagerFactory);
   }
 
   @Test
@@ -38,9 +50,8 @@ public class TopicDAOTest extends BaseTest {
   public void remove() {
     Topic topic = new Topic("Math", "From algebra to calculus.");
 
-    topicDAO.persist(topic);
+    topic = topicDAO.merge(topic);
     assertNotNull(topicDAO.findById(topic.getId()));
-
     topicDAO.remove(topic);
     assertNull(topicDAO.findById(topic.getId()));
   }
@@ -58,8 +69,16 @@ public class TopicDAOTest extends BaseTest {
   public void findSingleTopicByTitle() {
     Topic topic = new Topic("Math", "From algebra to calculus.");
     topicDAO.persist(topic);
+    Topic result = topicDAO.findSingleTopicByTitle("Math");
+    assertEquals(result, topic);
+  }
 
-    assertEquals(topicDAO.findSingleTopicByTitle("Math"), topic);
+  @Test
+  public void findTopicsByTitle() {
+    Topic topic = new Topic("Math", "From algebra to calculus.");
+    topicDAO.merge(topic);
+    List<Topic> result = topicDAO.findtopicByTitle("Math");
+    assertTrue(result.contains(topic));
   }
 
 }
