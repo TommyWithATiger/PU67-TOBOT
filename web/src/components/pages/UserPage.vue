9a07ea5ba962c623521c64963e64b1502c8c6343
Niklas Molnes Hole <nikasmh@hotmail.com>
2017-02-22 13:06:26 +0100

    Added a get user method to userpage

--- web/src/components/pages/UserPage.vue
+++ web/src/components/pages/UserPage.vue
@@ -2,28 +2,36 @@
   <div class="page-content">
     <h1 v-if="fullName.length">{{ fullName }}</h1>
     <h1 v-else>User has no name</h1>
-    <p>Username: {{ user.username }}</p>
+    <p>Username: {{ username }}</p>
   </div>
 </template>
 
 <script>
+import { api } from 'api'
+
 export default {
   name: 'userpage',
   data () {
     return {
       user: {
-        username: '',
         firstName: '',
         lastName: ''
       }
     }
   },
   created () {
-    this.user.username = this.$store.state.user.username
+    api.getUser(this.user.username, (result) => {
+      this.$store.state.user.username = result.username
+      this.firstName = result.firstName
+      this.lastName = result.lastName
+    })
   },
   computed: {
     fullName () {
       return `${this.user.firstName || ''} ${this.user.lastName || ''}`.trim()
+    },
+    username () {
+      return this.$store.state.user.username
     }
   }
 }
