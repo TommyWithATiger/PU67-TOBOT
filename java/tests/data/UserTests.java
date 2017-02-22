cec0436d674d2c2920b9aaa3b6127f119114bd68
Espen Haugsdal <espen.haugsdal@gmail.com>
2017-02-22 17:22:58 +0100

    Added tests for session related methods
    Also fixed errors causing the tests to fail.

--- java/tests/data/UserTests.java
+++ java/tests/data/UserTests.java
@@ -5,6 +5,8 @@ import static junit.framework.TestCase.assertFalse;
 import static junit.framework.TestCase.assertTrue;
 
 import base.BaseTest;
+import java.util.Calendar;
+import java.util.Date;
 import org.junit.Test;
 
 public class UserTests extends BaseTest {
@@ -28,5 +30,47 @@ public class UserTests extends BaseTest {
     user.setPassword("hunter2");
     assertTrue(user.checkPassword("hunter2"));
     assertFalse(user.checkPassword("password123"));
+  }
+
+  @Test
+  public void testGenerateSessionToken() throws Exception {
+    User user = new User();
+    user.createSessionToken();
+    assertEquals(64, user.getSessionToken().length());
+  }
+
+  @Test
+  public void testGenerateSessionTokenExpireDate() throws Exception {
+    User user = new User();
+    user.generateSessionTokenExpireDate();
+    assertTrue(user.getSessionTokenExpireDate().after(new Date()));
+  }
+
+  @Test
+  public void testCheckUserSessionToken1() throws Exception {
+    User user = new User();
+    assertFalse(user.checkUserSessionToken("garbage"));
+  }
+
+  @Test
+  public void testCheckUserSessionToken2() throws Exception {
+    User user = new User();
+    user.createSessionToken();
+    assertTrue(user.checkUserSessionToken(user.getSessionToken()));
+  }
+
+  @Test
+  public void testCheckUserSessionToken3() throws Exception {
+    User user = new User();
+    user.createSessionToken();
+
+    Calendar calendar = Calendar.getInstance();
+    calendar.setTime(new Date());
+    calendar.add(Calendar.DATE, -1);
+    user.setSessionTokenExpireDate(calendar.getTime());
+
+    assertFalse(user.checkUserSessionToken(user.getSessionToken()));
+    assertEquals(null, user.getSessionTokenExpireDate());
+    assertEquals(null, user.getSessionToken());
   }
 }
