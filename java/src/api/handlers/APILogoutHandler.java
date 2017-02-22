2427efb6a45d0ad4126f95f1bf17d2be0984b465
TommyWithATiger <halvard.hummel@hotmail.com>
2017-02-22 13:26:24 +0100

    Created the base for the API handlers for checking login and logging out

--- java/src/api/handlers/APILogoutHandler.java
+++ java/src/api/handlers/APILogoutHandler.java
@@ -1,12 +1,32 @@
 package api.handlers;
 
+import static api.helpers.EntityContentHelper.checkAndGetEntityContent;
+import static api.helpers.RequestMethodHelper.checkRequestMethod;
+
+import api.exceptions.APIBadRequestException;
 import org.apache.http.HttpRequest;
+import org.json.JSONObject;
 
 public class APILogoutHandler {
 
   public static String handleLogoutRequest(HttpRequest httpRequest){
+    checkRequestMethod("POST", httpRequest);
 
-    return "";
+    String requestContent = checkAndGetEntityContent(httpRequest);
+
+    JSONObject jsonObject = new JSONObject(requestContent);
+
+    if (!jsonObject.has("username") || !jsonObject.has("token")){
+      throw new APIBadRequestException("Logout data not complete");
+    }
+
+    String username = jsonObject.getString("username");
+    String password = jsonObject.getString("password");
+
+    JSONObject logoutResponse = new JSONObject();
+    logoutResponse.put("logged_out", "true");
+
+    return logoutResponse.toString();
   }
 
 }
