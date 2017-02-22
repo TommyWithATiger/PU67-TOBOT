942f60bde6e03306fb994a494ff95a94ed68136c
TommyWithATiger <halvard.hummel@hotmail.com>
2017-02-22 12:15:59 +0100

    Made basic handlers for login, logout and checking if logged in

---
+++ java/src/api/handlers/APILogoutHandler.java
@@ -0,0 +1,12 @@
+package api.handlers;
+
+import org.apache.http.HttpRequest;
+
+public class APILogoutHandler {
+
+  public static String handleLogoutRequest(HttpRequest httpRequest){
+
+    return "";
+  }
+
+}
