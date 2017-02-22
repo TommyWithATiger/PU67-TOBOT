49f664f17f2f353ee5a30f68bac055807dcfba65
Niklas Molnes Hole <nikasmh@hotmail.com>
2017-02-22 11:52:12 +0100

    Added user page component

---
+++ web/src/components/pages/UserPage.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="page-content">
+    <h1>{{ fullName }}</h1>
+    <p>Username: {{ user.username }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'userpage',
+  data () {
+    return {
+      user: {
+        username: '',
+        firstName: '',
+        lastName: ''
+      }
+    }
+  },
+  computed: {
+    fullName () {
+      return `${this.firstName} ${this.lastName}`
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
