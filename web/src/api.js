1ef0ca751ffefbbbe816f20e5e781e362b967285
Niklas Molnes Hole <nikasmh@hotmail.com>
2017-02-23 11:44:52 +0100

    Added addTopic mathod in API

--- web/src/api.js
+++ web/src/api.js
@@ -1,6 +1,8 @@
 import { API_URL } from './constants'
 // const USER_URL = `${API_URL}/user/`
 const LOGIN_URL = `${API_URL}/user/login`
+// const TOPIC_GET_URL = `${API_URL}/topic/get`
+const TOPIC_ADD_URL = `${API_URL}/topic/create`
 
 export const api = {
   /**
@@ -31,8 +33,41 @@ export const api = {
       mode: 'no-cors'
     }
 
+    this.postRequest(LOGIN_URL, req, callback, error)
+  }
+
+  /**
+   * Add topic.
+   * @param {object} ctx Context.
+   * @param {object} data The data to post in request.
+   */
+  addTopic (ctx, topic, callback, error) {
+    // Here we can inject token.
+
+    let data = {
+      title: topic.title
+      description: topic.description
+    }
+
+    let req = {
+      body: JSON.stringify(data),
+      method: 'POST',
+      mode: 'no-cors'
+    }
+
+    postRequest (TOPIC_ADD_URL, req, callback, error) {
+  }
+
+  /**
+   * General function for posting requests to the API.
+   * @param {string} url URL address to send to.
+   * @param {object} req Data to send with the post request.
+   * @param {function} callback Handle the request output.
+   * @param {function} error Feedback error.
+   */
+  postRequest (url, req, callback, error) {
     // ctx.$http.post(LOGIN_URL, req)
-    fetch(LOGIN_URL, req) // Does not work in IE, needs polyfill.
+    fetch(url, req) // Does not work in IE, needs polyfill.
     .then(res => res.json())
     .then(callback)
     .catch(error)
