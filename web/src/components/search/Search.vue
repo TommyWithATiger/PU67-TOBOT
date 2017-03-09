c6f698fccded1688902a82e5daad6569c5357b3b
Niklas Molnes Hole <nikasmh@hotmail.com>
2017-03-09 20:15:09 +0100

    Added a search component giving out results

---
+++ web/src/components/search/Search.vue
@@ -0,0 +1,62 @@
+<template>
+  <input v-model="search" v-on:input="searchHandler" />
+</template>
+
+<script>
+import Rx from 'rxjs/Rx'
+
+import { api } from 'api'
+
+export default {
+  name: 'searchbar',
+  data () {
+    return {
+      search: ''
+    }
+  },
+  methods: {
+    searchHandler () {
+      this.$emit('search', this.search)
+      if (!this.search.length) {
+        this.$emit('subjectResult', [])
+        this.$emit('topicResult', [])
+      }
+    }
+  },
+  subscriptions () {
+    return {
+      subjectResult$: this.$watchAsObservable('search')
+        .pluck('newValue')
+        .filter(term => term.length > 1)
+        .debounceTime(100)
+        .distinctUntilChanged()
+        .switchMap((term) => {
+          return Rx.Observable.fromPromise(
+            api.getSubjectsByTitle(this, term.trim())
+          )
+        })
+        .map((res) => {
+          this.$emit('subjectResult', res.subjects.slice(0, 3))
+          return res.subjects.slice(0, 3)
+        }),
+      topicResult$: this.$watchAsObservable('search')
+        .pluck('newValue')
+        .filter(term => term.length > 1)
+        .debounceTime(100)
+        .distinctUntilChanged()
+        .switchMap((term) => {
+          return Rx.Observable.fromPromise(
+            api.getTopicsByTitle(this, term.trim())
+          )
+        })
+        .map((res) => {
+          this.$emit('topicResult', res.topics.slice(0, 3))
+          return res.topics.slice(0, 3)
+        })
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
