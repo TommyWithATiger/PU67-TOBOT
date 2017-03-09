432db0df510fa62f99fd433f6308cf92ee8f71fb
Niklas Molnes Hole <nikasmh@hotmail.com>
2017-03-09 04:31:43 +0100

    Added spesific user dashboards to front page

--- web/src/components/pages/FrontPage.vue
+++ web/src/components/pages/FrontPage.vue
@@ -3,10 +3,17 @@
     <h1 v-if="username">Hei, {{ username }}!</h1>
     <h1 v-else>Velkommen</h1>
     <p v-if="usertype">Du har rollen som {{ usertype }}</p>
+    <StudentDashboard v-if="isStudent" />
+    <AdminDashboard v-if="isAdmin" />
+    <TeacherDashboard v-if="isTeacher" />
   </div>
 </template>
 
 <script>
+import AdminDashboard from 'components/users/admin/AdminDashboard'
+import StudentDashboard from 'components/users/student/StudentDashboard'
+import TeacherDashboard from 'components/users/teacher/TeacherDashboard'
+
 export default {
   name: 'frontpage',
   computed: {
@@ -15,7 +22,21 @@ export default {
     },
     usertype () {
       return this.$store.state.user.usertype
+    },
+    isAdmin () {
+      return this.$store.state.user.usertype === 'Admin'
+    },
+    isTeacher () {
+      return this.$store.state.user.usertype === 'Teacher'
+    },
+    isStudent () {
+      return this.$store.state.user.usertype === 'Student'
     }
+  },
+  components: {
+    AdminDashboard,
+    StudentDashboard,
+    TeacherDashboard
   }
 }
 </script>
