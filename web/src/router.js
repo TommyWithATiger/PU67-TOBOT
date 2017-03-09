5dbe6e928a77bd904b5f75d2651a44188ba746d1
Niklas Molnes Hole <nikasmh@hotmail.com>
2017-03-09 15:54:58 +0100

    Added more validation before each pagination

--- web/src/router.js
+++ web/src/router.js
@@ -3,6 +3,7 @@ import Router from 'vue-router'
 
 import FrontPage from 'components/pages/FrontPage'
 import LoginPage from 'components/pages/LoginPage'
+import RestrictedPage from 'components/pages/RestrictedPage'
 import UserPage from 'components/pages/UserPage'
 import TopicPage from 'components/pages/TopicPage'
 import SubjectPage from 'components/pages/SubjectPage'
@@ -21,29 +22,81 @@ Vue.use(Router)
  */
 function requireAuth (to, from, next) {
   if (auth.hasToken()) {
+    if (/^\/login/.test(from.path)) {
+      store.state.user.username = ''
+      store.state.user.usertype = ''
+    }
+
     auth.isAuth(() => {
-      api.getUser(this, (data) => {
-        store.state.user.username = data.username
-        store.state.user.usertype = data.userType
-        store.state.user.email = data.email
-      })
-      next()
-    }, () => {
-      console.log(24)
-      next({
-        path: '/login',
-        query: {
-          redirect: to.fullPath
+      if (/^\/login/.test(from.path)) {
+        api.getUser(this, (data) => {
+          store.state.user.username = data.username
+          store.state.user.usertype = data.userType
+          store.state.user.email = data.email
+          if (to.meta.users.indexOf(data.userType) !== -1) {
+            next()
+          } else {
+            next(getRestrictedRoute(to))
+          }
+        })
+      } else {
+        api.getUser(this, (data) => {
+          store.state.user.username = data.username
+          store.state.user.usertype = data.userType
+          store.state.user.email = data.email
+        })
+
+        if (to.meta.users.indexOf(store.state.user.usertype) !== -1) {
+          next()
+        } else {
+          if (store.state.user.usertype === 'undefined') {
+            api.getUser(this, (data) => {
+              store.state.user.username = data.username
+              store.state.user.usertype = data.userType
+              store.state.user.email = data.email
+
+              if (to.meta.users.indexOf(store.state.user.usertype) !== -1) {
+                next()
+              } else {
+                next(getRestrictedRoute(to))
+              }
+            })
+          } else {
+            next(getRestrictedRoute(to))
+          }
         }
-      })
+      }
+    }, () => {
+      next(getLoginRoute(to))
     })
   } else {
-    next({
-      path: '/login',
-      query: {
-        redirect: to.fullPath
-      }
-    })
+    next(getLoginRoute(to))
+  }
+}
+
+/**
+ * Get the next route for restricted login.
+ * @param {object} to The route to go to. Needs fullPath.
+ */
+function getRestrictedRoute (to) {
+  return {
+    path: '/restricted',
+    query: {
+      redirect: to.fullPath
+    }
+  }
+}
+
+/**
+ * Get the next route for login.
+ * @param {object} to The route to go to. Needs fullPath.
+ */
+function getLoginRoute (to) {
+  return {
+    path: '/login',
+    query: {
+      redirect: to.fullPath
+    }
   }
 }
 
@@ -54,30 +107,35 @@ export const router = new Router({
       path: '/',
       name: 'Home',
       component: FrontPage,
+      meta: { users: [ 'Admin', 'Teacher', 'Student' ] },
       beforeEnter: requireAuth
     },
     {
       path: '/subject',
-      name: 'Subject',
+      name: 'Subjects',
       component: SubjectPage,
+      meta: { users: [ 'Admin', 'Teacher' ] },
       beforeEnter: requireAuth
     },
     {
       path: '/topic',
       name: 'Topics',
       component: TopicPage,
+      meta: { users: [ 'Admin' ] },
       beforeEnter: requireAuth
     },
     {
       path: '/subject/:id',
       name: 'RelateSubjectTopic',
       component: RelateSubjectTopicPage,
+      meta: { users: [ 'Admin', 'Teacher' ] },
       beforeEnter: requireAuth
     },
     {
       path: '/user/:user',
       name: 'User',
       component: UserPage,
+      meta: { users: [ 'Admin', 'Teacher', 'Student' ] },
       beforeEnter: requireAuth
     },
     {
@@ -86,9 +144,15 @@ export const router = new Router({
       component: LoginPage
     },
     {
+      path: '/restricted',
+      name: 'Restricted',
+      component: RestrictedPage
+    },
+    {
       path: '/*',
       name: 'Default',
       component: FrontPage,
+      meta: { users: [ 'Admin', 'Teacher', 'Student' ] },
       beforeEnter: (to, from, next) => {
         next('/')
       }
