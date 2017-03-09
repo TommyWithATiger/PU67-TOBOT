28d416fe0b8425eebaf9a692f778380e58e629b3
Niklas Molnes Hole <nikasmh@hotmail.com>
2017-03-09 15:54:13 +0100

    Made a restricted page for unauthed locations

---
+++ web/src/components/pages/RestrictedPage.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="page-content">
+    <h1>Ingen adgang. Logg inn med annen bruker:</h1>
+    <div>
+      <input @keydown.enter="login" v-model="creds.username" placeholder="Brukernavn ..." type="text" />
+      <input @keydown.enter="login" v-model="creds.password" placeholder="Passord ..." type="password" />
+      <button @click="login">Logg inn</button>
+      <span>{{ feedback }}</span>
+    </div>
+  </div>
+</template>
+
+<script>
+import { auth } from 'auth'
+
+export default {
+  name: 'restrictedpage',
+  data () {
+    return {
+      creds: {
+        username: '',
+        password: ''
+      },
+      feedback: ''
+    }
+  },
+  methods: {
+    successHandler (feedback) {
+      this.feedback = feedback
+    },
+    errorHandler (feedback) {
+      this.feedback = feedback
+    },
+    /**
+     * Send a request to the login URL and save the returned JWT.
+     */
+    login () {
+      if (this.creds.username.length) {
+        this.feedback = 'Sjekker validering ...'
+        auth.login(this.creds, this, this.$route.query.redirect || '/', () => {
+          this.successHandler('Valid login.')
+        }, (err) => {
+          if (typeof err === 'string') this.errorHandler(err)
+        })
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
