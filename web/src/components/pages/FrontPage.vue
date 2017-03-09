7e88a4c0ca40f0b6c05aa69f43550c13cec30c82
Niklas Molnes Hole <nikasmh@hotmail.com>
2017-03-09 02:28:14 +0100

    Added userinfo on frontpage

--- web/src/components/pages/FrontPage.vue
+++ web/src/components/pages/FrontPage.vue
@@ -1,16 +1,25 @@
 <template>
   <div class="page-content">
-    <h1>{{ title }}</h1>
+    <h1 v-if="username">Hei, {{ username }}!</h1>
+    <h1 v-else>Velkommen</h1>
+    <p v-if="usertype">Du har rollen som {{ usertype }}</p>
   </div>
 </template>
 
 <script>
+import { auth } from 'auth'
+
 export default {
   name: 'frontpage',
   data () {
     return {
-      title: 'Velkommen til TOBOT'
+      username: '',
+      usertype: ''
     }
+  },
+  created () {
+    this.username = auth.getUsername()
+    this.usertype = auth.getUsertype()
   }
 }
 </script>
