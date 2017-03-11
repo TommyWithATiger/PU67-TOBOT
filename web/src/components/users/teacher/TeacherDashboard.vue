bd113602fdc6d479172b724557d0b9a1f303602d
Niklas Molnes Hole <nikasmh@hotmail.com>
2017-03-11 15:32:27 +0100

    Added statistics to teacher dashboard

--- web/src/components/users/teacher/TeacherDashboard.vue
+++ web/src/components/users/teacher/TeacherDashboard.vue
@@ -1,10 +1,24 @@
 <template>
-  <h1>Lærer</h1>
+  <div>
+    <h1 v-if="username">Hei, {{ username }}!</h1>
+    <h1 v-else>Hei</h1>
+    <StudentStatistics />
+  </div>
 </template>
 
 <script>
+import StudentStatistics from 'components/users/teacher/StudentStatistics'
+
 export default {
-  name: 'teacerdashboard'
+  name: 'teacerdashboard',
+  computed: {
+    username () {
+      return this.$store.state.user.username
+    }
+  },
+  components: {
+    StudentStatistics
+  }
 }
 </script>
 
