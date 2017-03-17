4186d35edbc8c78462855800517321b0ef5eb8ea
Niklas Molnes Hole <nikasmh@hotmail.com>
2017-03-17 05:57:13 +0100

    Fixed registration page

--- web/src/components/pages/RegisterPage.vue
+++ web/src/components/pages/RegisterPage.vue
@@ -3,25 +3,46 @@
     <div class="form-content">
       <div class="form-row">
         <label for="username">Username: </label>
-        <input @focusout="checkRegistration" @keydown.enter="register" placeholder="Username" v-model="username" id="username" type="text">
-        <div class="inputError" id="username-error"></div>
+        <input
+          @focusout="checkRegistration"
+          @keydown.enter="register"
+          placeholder="Username"
+          v-model="username"
+          id="username"
+          :class="usernameValid ? '' : 'invalid-input'"
+          type="text">
+        <div class="inputError">{{ usernameError }}</div>
       </div>
       <div class="form-row">
         <label for="password">Password: </label>
-        <input @focusout="checkRegistration" @keydown.enter="register" placeholder="Password" v-model="password" id="password" type="password">
-        <div class="inputError" id="password-error"></div>
+        <input
+          @focusout="checkRegistration"
+          @keydown.enter="register"
+          placeholder="Password"
+          v-model="password"
+          id="password"
+          :class="passwordValid ? '' : 'invalid-input'"
+          type="password">
+        <div class="inputError">{{ passwordError }}</div>
       </div>
       <div class="form-row">
         <label for="email">E-mail: </label>
-        <input @focusout="checkRegistration" @keydown.enter="register" placeholder="E-mail" v-model="email" id="email" type="email">
-        <div class="inputError" id="email-error"></div>
+        <input
+          @focusout="checkRegistration"
+          @keydown.enter="register"
+          placeholder="E-mail"
+          v-model="email"
+          id="email"
+          :class="emailValid ? '' : 'invalid-input'"
+          type="email">
+        <div class="inputError">{{ emailError }}</div>
       </div>
       <div class="form-row userType">
-        <input v-model="userType" type="radio" name="userType" value="Student"> Student
-        <input v-model="userType" type="radio" name="userType" value="Teacher"> Teacher 
+        <input v-model="userType" type="radio" id="student" value="Student"><label for="student">Student</label>
+        <input v-model="userType" type="radio" id="teacher" value="Teacher"><label for="teacher">Teacher</label>
       </div>
       <div class="form-row">
-        <button class="registerButton" @click="register">Registrer</button>
+        <button class="registerButton" @click="register" :disabled="canSubmit">Registrer</button>
       </div>
     </div>
   </div>
@@ -38,37 +59,30 @@ export default {
       username: '',
       password: '',
       email: '',
+      usernameValid: true,
+      passwordValid: true,
+      emailValid: true,
       userType: 'Student',
-      canSubmit: false
+      canSubmit: false,
+      usernameError: '',
+      passwordError: '',
+      emailError: ''
     }
   },
   methods: {
     checkRegistration () {
       api.checkRegistrationData(this, this.username, this.email, this.password, (data) => {
-        console.log(data)
         if (data.username_valid && data.password_valid && data.email_valid) {
           this.canSubmit = true
         }
-        document.getElementById('username').classList.toggle('invalidInput', !data.username_valid)
-        document.getElementById('password').classList.toggle('invalidInput', !data.password_valid)
-        document.getElementById('email').classList.toggle('invalidInput', !data.email_valid)
-        if (!data.username_valid) {
-          document.getElementById('username-error').textContent = data.username_message
-        } else {
-          document.getElementById('username-error').textContent = ''
-        }
-        if (!data.password_valid) {
-          document.getElementById('password-error').textContent = data.password_message
-        } else {
-          document.getElementById('password-error').textContent = ''
-        }
-        if (!data.email_valid) {
-          document.getElementById('email-error').textContent = data.email_message
-        } else {
-          document.getElementById('email-error').textContent = ''
-        }
-      }, (err) => {
-        console.log(err)
+
+        this.usernameValid = data.username_valid
+        this.passwordValid = data.password_valid
+        this.emailValid = data.email_valid
+
+        this.usernameError = data.username_valid ? '' : data.username_message
+        this.passwordError = data.password_valid ? '' : data.password_message
+        this.emailError = data.email_valid ? '' : data.email_message
       })
     },
     register () {
@@ -79,13 +93,9 @@ export default {
             username: this.username,
             password: this.password
           }, this, '/', () => {
-
-          }, (err) => {
-            console.log(err)
+          }, () => {
             this.$router.push('/login')
           })
-        }, (err) => {
-          console.log(err)
         })
       }
     }
@@ -116,19 +126,17 @@ export default {
 	font-weight: bold;
 }
 
-.form-row > input[type="text"].invalidInput, .form-row > input[type="password"].invalidInput, .form-row > input[type="email"].invalidInput{
+.form-row > input[type="text"].invalid-input,
+.form-row > input[type="password"].invalid-input,
+.form-row > input[type="email"].invalid-input {
 	border-color: red;
 }
 
-.form-row > input[type="text"], .form-row > input[type="password"], .form-row > input[type="email"]  {
-	height: 25px;
-	width: 285px;
-	border: 1px solid gray;
-	border-radius: 7px;
-	flow: right;
-	padding: 2px;
-	font-size: 15px;
+.form-row > input[type="text"],
+.form-row > input[type="password"],
+.form-row > input[type="email"] {
 	text-align: center;
+  width: 100%;
 }
 
 .userType {
