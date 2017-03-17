6c2d8698f54a36c9a45027b1146f258df8d2d6a0
Niklas Molnes Hole <nikasmh@hotmail.com>
2017-03-17 01:41:50 +0100

    Added theme to site

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="app" class="app">
+  <div id="app" :class="`app secondary-background-color secondary-color ${theme}`">
     <TopBar></TopBar>
     <router-view></router-view>
   </div>
@@ -10,6 +10,11 @@ import TopBar from 'components/template/TopBar'
 
 export default {
   name: 'app',
+  computed: {
+    theme () {
+      return this.$store.state.theme
+    }
+  },
   components: {
     TopBar
   }
@@ -17,9 +22,31 @@ export default {
 </script>
 
 <style>
+:root {
+  --primary-color: #9ff;
+  --primary-color-dark: #0ff;
+  --secondary-color: #444;
+  --secondary-color-dark: #eee;
+
+  --primary-background-color: #fff;
+  --primary-background-color-dark: #333;
+  --secondary-background-color: #eee;
+  --secondary-background-color-dark: #444;
+}
+
+.primary-background-color, .primary-background-color.light { background-color: #9ff; background-color: var(--primary-background-color); }
+.dark .primary-background-color, .primary-background-color.dark { background-color: #0ff; background-color: var(--primary-background-color-dark); }
+
+.secondary-background-color, .secondary-background-color.light { background-color: #eee; background-color: var(--secondary-background-color); }
+.dark .secondary-background-color, .secondary-background-color.dark { background-color: #444; background-color: var(--secondary-background-color-dark); }
+
+.primary-color, .primary-color.light { color: #9ff; color: var(--primary-color); }
+.dark .primary-color, .primary-color.dark { color: #0ff; color: var(--primary-color-dark); }
+
+.secondary-color, .secondary-color.light { color: #eee; color: var(--secondary-color); }
+.dark .secondary-color, .secondary-color.dark { color: #444; color: var(--secondary-color-dark); }
+
 .app {
-  background-color: #ddd;
-  color: #333;
   display: flex;
   flex-direction: column;
   min-height: 100vh;
@@ -35,9 +62,13 @@ export default {
 }
 
 .content-container,
+.header-container,
 .page-content {
   max-width: 960px;
   width: 100%;
   margin: auto;
+}
+.header-container {
+  max-width: 1200px;
 }
 </style>
