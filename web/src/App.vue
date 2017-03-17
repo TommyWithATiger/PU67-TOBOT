465d78c31e0d013d6133c6d1f11a9fe539faab5e
Niklas Molnes Hole <nikasmh@hotmail.com>
2017-03-17 02:17:09 +0100

    Added color system for input and buttons

--- web/src/App.vue
+++ web/src/App.vue
@@ -32,6 +32,18 @@ export default {
   --primary-background-color-dark: #333;
   --secondary-background-color: #eee;
   --secondary-background-color-dark: #444;
+
+  --button-background-color: #0df;
+  --button-background-color-dark: #0df;
+  --button-hover-background-color: #0bd;
+  --button-hover-background-color-dark: #0bd;
+  --button-disabled-background-color: #999;
+  --button-disabled-background-color-dark: #999;
+
+  --input-background-color: #ddd;
+  --input-background-color-dark: #555;
+  --input-color: #555;
+  --input-color-dark: #ddd;
 }
 
 .primary-background-color, .primary-background-color.light { background-color: #9ff; background-color: var(--primary-background-color); }
@@ -70,5 +82,82 @@ export default {
 }
 .header-container {
   max-width: 1200px;
+}
+
+.dark button,
+.dark input[type=submit],
+.dark input[type=reset] {
+  background-color: #0df;
+  background-color: var(--button-background-color-dark);
+}
+
+button,
+input[type=submit],
+input[type=reset] {
+  background-color: #0df;
+  background-color: var(--button-background-color);
+  border: none;
+  padding: 8px 16px;
+  cursor: pointer;
+  border-radius: 3px;
+  outline: none;
+  box-shadow: 0 0 8px -2px #000;
+  transition-property: background-color box-shadow;
+  transition-duration: .2s;
+}
+
+.dark button:hover,
+.dark input[type=submit]:hover,
+.dark input[type=reset]:hover {
+  background-color: #0bd;
+  background-color: var(--button-hover-background-color-dark);
+}
+
+button:hover,
+input[type=submit]:hover,
+input[type=reset]:hover {
+  background-color: #0bd;
+  background-color: var(--button-hover-background-color);
+  transition-duration: .05s;
+}
+
+button:active,
+input[type=submit]:active,
+input[type=reset]:active {
+  box-shadow: none;
+  transition-duration: .05s;
+}
+
+.dark button[disabled],
+.dark input[type=submit][disabled],
+.dark input[type=reset][disabled] {
+  background-color: #0bd;
+  background-color: var(--button-background-color-dark);
+}
+button[disabled],
+input[type=submit][disabled],
+input[type=reset][disabled] {
+  background-color: #0bd;
+  background-color: var(--button-background-color);
+}
+
+.dark input[type=text],
+.dark input {
+  background-color: #999;
+  background-color: var(--input-background-color-dark);
+  color: #555;
+  color: var(--input-color-dark);
+}
+
+input[type=text],
+input {
+  background-color: #555;
+  background-color: var(--input-background-color);
+  color: #999;
+  color: var(--input-color);
+  outline: none;
+  border: none;
+  border-radius: 4px;
+  padding: 8px 16px;
 }
 </style>
