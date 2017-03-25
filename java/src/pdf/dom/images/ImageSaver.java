213c6c6de4e418cbaec27766a6c079fe43631931
TommyWithATiger <halvard.hummel@hotmail.com>
2017-03-25 11:32:49 +0100

    Image saver is no a thread and image extractor spawns a process for each one. This means that all the image uploads to the image host is done in parrallel. For the test PDF I used which earlier took around 60 seconds, the improved upload time is now around 5 seconds

--- java/src/pdf/dom/images/ImageSaver.java
+++ java/src/pdf/dom/images/ImageSaver.java
@@ -1,29 +1,43 @@
 package pdf.dom.images;
 
+import api.exceptions.APIBadRequestException;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.net.InetSocketAddress;
 import java.net.Socket;
-import org.apache.http.Header;
 import org.apache.http.HttpException;
 import org.apache.http.HttpResponse;
 import org.apache.http.impl.io.DefaultHttpResponseParser;
 import org.apache.http.impl.io.HttpTransportMetricsImpl;
 import org.apache.http.impl.io.SessionInputBufferImpl;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
 
-public class ImageSaver {
+public class ImageSaver extends Thread {
 
   public static final String BASEURL = "http://cdn.tobot.hummel.io:8080/images/";
+  private String base64;
+  private Node imageNode;
 
-  public static String saveBase64Image(String base64) {
+  public ImageSaver(String base64, Node imageNode) {
+    this.base64 = base64;
+    this.imageNode = imageNode;
+  }
+
+  @Override
+  public void run() {
+    ((Element) imageNode).setAttribute("src", saveBase64Image(base64));
+  }
+
+  private String saveBase64Image(String base64) {
     try {
       return makeSaveRequest(base64.substring(base64.indexOf(",") + 1));
     } catch (IOException | HttpException e) {
-      return null;
+      throw new APIBadRequestException("Could not handle the image");
     }
   }
 
-  private static String makeSaveRequest(String base64Image) throws IOException, HttpException {
+  private String makeSaveRequest(String base64Image) throws IOException, HttpException {
     Socket socket = new Socket();
     socket.connect(new InetSocketAddress("cdn.tobot.hummel.io", 8080));
 
