03f0ea71475889abbecbc53e957f74695280b76e
TommyWithATiger <halvard.hummel@hotmail.com>
2017-03-30 18:06:33 +0200

    Another test also required this to function properly

--- java/tests/http/HTTPHandlerTest.java
+++ java/tests/http/HTTPHandlerTest.java
@@ -3,6 +3,7 @@ package http;
 import static junit.framework.TestCase.assertEquals;
 import static junit.framework.TestCase.fail;
 
+import base.IndexPageTest;
 import base.ServerBaseTest;
 import java.io.File;
 import java.io.IOException;
@@ -13,34 +14,7 @@ import org.junit.Before;
 import org.junit.Test;
 import server.http.HttpFileHandler;
 
-public class HTTPHandlerTest extends ServerBaseTest {
-
-  private File deleteFile;
-
-  @Before
-  public void before() {
-    File file = new File(HttpFileHandler.basePath + "index.html");
-    if (!file.exists()) {
-      File parent = file.getParentFile();
-      if (!parent.exists()) {
-        deleteFile = parent;
-        while (!deleteFile.getParentFile().exists()) {
-          deleteFile = deleteFile.getParentFile();
-        }
-        if (!parent.mkdirs()) {
-          fail("Could not create required folder structure");
-        }
-      }
-
-
-      try {
-        file.createNewFile();
-        deleteFile = file;
-      } catch (IOException e) {
-        fail("Could not create file needed for test");
-      }
-    }
-  }
+public class HTTPHandlerTest extends IndexPageTest {
 
   @Test
   public void testRedirectOnInvalidUri() throws IOException, HttpException {
@@ -49,13 +23,6 @@ public class HTTPHandlerTest extends ServerBaseTest {
 
     HttpResponse httpResponse = responseParser.parse();
     assertEquals("HTTP/1.1 200 OK", httpResponse.getStatusLine().toString());
-  }
-
-  @After
-  public void after() {
-    if (deleteFile != null) {
-      deleteFile.delete();
-    }
   }
 
 
