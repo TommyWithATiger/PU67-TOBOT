537938fb5425b71a5b4bdb05dc07cde79b49a059
Espen Haugsdal <espen.haugsdal@gmail.com>
2017-03-31 08:35:46 +0200

    Add ExerciseRatingEnum test

---
+++ java/tests/data/ExerciseRatingEnumTest.java
@@ -0,0 +1,20 @@
+package data;
+
+import static org.junit.Assert.assertEquals;
+
+import base.BaseTest;
+import data.exerciserating.ExerciseRatingEnum;
+import org.junit.Test;
+
+public class ExerciseRatingEnumTest extends BaseTest {
+
+  @Test
+  public void testGet() throws Exception {
+    assertEquals(ExerciseRatingEnum.Easy, ExerciseRatingEnum.get(1));
+    assertEquals(ExerciseRatingEnum.Medium, ExerciseRatingEnum.get(2));
+    assertEquals(ExerciseRatingEnum.Hard, ExerciseRatingEnum.get(3));
+    assertEquals(ExerciseRatingEnum.Unknown, ExerciseRatingEnum.get(4));
+    assertEquals(ExerciseRatingEnum.Unknown, ExerciseRatingEnum.get(1000));
+  }
+
+}
