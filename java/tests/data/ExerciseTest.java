5609f1cc0e6d2511e65a6eb0365a06cd89dc9e26
Espen Haugsdal <espen.haugsdal@gmail.com>
2017-04-05 20:10:19 +0200

    add testCreateAbout

--- java/tests/data/ExerciseTest.java
+++ java/tests/data/ExerciseTest.java
@@ -10,6 +10,7 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import org.apache.commons.collections.CollectionUtils;
+import org.json.JSONObject;
 import org.junit.Test;
 
 public class ExerciseTest extends BaseTest {
@@ -68,7 +69,29 @@ public class ExerciseTest extends BaseTest {
     Exercise exercise1 = ExerciseDAO.getInstance().findById(exercise.getId());
     assertTrue(CollectionUtils.isEqualCollection(topic1.getExercises(), Collections.singletonList(exercise1)));
     assertTrue(CollectionUtils.isEqualCollection(topic2.getExercises(), Collections.singletonList(exercise1)));
+  }
 
+
+  @Test
+  public void testCreateAbout() throws Exception {
+    Exercise exercise = new Exercise("Title", "<html stuff/>", "<html stuff solution/>");
+    exercise.create();
+
+    Topic topic1 = new Topic("Title1", "description1");
+    topic1.create();
+    Topic topic2 = new Topic("Title2", "description2");
+    topic2.create();
+    List<Topic> topics = new ArrayList<>();
+    topics.add(topic1);
+    topics.add(topic2);
+    exercise.addToTopics(topics);
+
+    JSONObject jsonExercise = ExerciseDAO.getInstance().findById(exercise.getId()).createAbout();
+    assertTrue(jsonExercise.has("id"));
+    assertEquals("Title", jsonExercise.getString("title"));
+    assertEquals("<html stuff/>", jsonExercise.getString("text"));
+    assertEquals("<html stuff solution/>", jsonExercise.getString("solution"));
+    assertEquals(2, jsonExercise.getJSONArray("tags").length());
   }
 
 }
