9f5b7378947c1704301d348429581ea5f2fe6dbd
Espen Haugsdal <espen.haugsdal@gmail.com>
2017-04-05 21:21:41 +0200

    removed unused set methods

--- java/src/data/rating/RatingKey.java
+++ java/src/data/rating/RatingKey.java
@@ -31,16 +31,8 @@ public class RatingKey implements Serializable {
     return userID;
   }
 
-  public void setUserID(Integer userID) {
-    this.userID = userID;
-  }
-
   public Integer getTopicID() {
     return topicID;
-  }
-
-  public void setTopicID(Integer topicID) {
-    this.topicID = topicID;
   }
 
   @Override
